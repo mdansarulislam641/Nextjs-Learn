04dece52c54f458a9684e79f2ff4e4b16b6b573a
mdansarulislam641 <mdansarulislam641.com>
2023-08-22 11:03:16 +0600

    nested routing explore DONE

---
+++ src/pages/books/[bookId].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookDetails = () => {
+    return (
+        <div>
+            <h1>This is book Details Page: </h1>
+        </div>
+    );
+};
+
+export default BookDetails;
