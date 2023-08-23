a551cfb098941d4a744538c0a7b42b2355af926f
mdansarulislam641 <mdansarulislam641.com>
2023-08-23 09:44:11 +0600

    navigate default router explore

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,13 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const ErrorPage = () => {
+    const router = useRouter();
+
+    setTimeout(()=>{
+        router.push('/')
+    },5000)
+
     return (
         <div>
             <img src="https://w7.pngwing.com/pngs/336/743/png-transparent-error-404-404-error.png" alt="error" 
--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,9 +1,14 @@
+import { Button } from 'antd';
+import Link from 'next/link';
 import React from 'react';
 
 const NewsPage = () => {
     return (
         <div>
             <h1>News Page</h1>
+            <Button type='primary'>
+                <Link href='/'>Home</Link>
+            </Button>
         </div>
     );
 };
