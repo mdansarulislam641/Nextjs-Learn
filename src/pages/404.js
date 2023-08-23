0e9ddefe48f7c41d9527509e9417c1941e555791
mdansarulislam641 <mdansarulislam641.com>
2023-08-23 09:23:42 +0600

    Catch all fintering router explore and ant design Install

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <img src="https://w7.pngwing.com/pngs/336/743/png-transparent-error-404-404-error.png" alt="error" 
+            width="100%"
+            />
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/pages/news/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CatchAllFilter = () => {
+    return (
+        <div>
+            <h1>this is catch all Filter</h1>
+        </div>
+    );
+};
+
+export default CatchAllFilter;
